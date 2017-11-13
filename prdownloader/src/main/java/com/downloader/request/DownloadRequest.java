bd1d18884ba1369c8e4291cb38068f767cfcf7b9
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2017-11-13 12:01:39 +0530

    Add request and request builder

---
+++ prdownloader/src/main/java/com/downloader/request/DownloadRequest.java
@@ -0,0 +1,81 @@
+/*
+ *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.downloader.request;
+
+import com.downloader.Priority;
+
+/**
+ * Created by amitshekhar on 13/11/17.
+ */
+
+public class DownloadRequest {
+
+    private Priority priority;
+    private Object tag;
+    private String url;
+    private String dirPath;
+    private String fileName;
+
+    public DownloadRequest(DownloadRequestBuilder downloadRequestBuilder) {
+        this.url = downloadRequestBuilder.url;
+        this.dirPath = downloadRequestBuilder.dirPath;
+        this.fileName = downloadRequestBuilder.fileName;
+        this.priority = downloadRequestBuilder.priority;
+        this.tag = downloadRequestBuilder.tag;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Object getTag() {
+        return tag;
+    }
+
+    public void setTag(Object tag) {
+        this.tag = tag;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirPath() {
+        return dirPath;
+    }
+
+    public void setDirPath(String dirPath) {
+        this.dirPath = dirPath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+}
