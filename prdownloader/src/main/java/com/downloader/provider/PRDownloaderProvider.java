1f4b40ac06c12e04b7fee663a6a71ea985faa9f6
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2017-11-14 11:44:11 +0530

    Add check for applicationId present or not

--- prdownloader/src/main/java/com/downloader/provider/PRDownloaderProvider.java
+++ prdownloader/src/main/java/com/downloader/provider/PRDownloaderProvider.java
@@ -1,8 +1,25 @@
+/*
+ *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.downloader.provider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.pm.ProviderInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -14,7 +31,7 @@ import com.downloader.PRDownloader;
  * WHY PREFIX PROVIDER AUTHORITY WITH "${applicationId}" :
  * Problem.
  * 1. In Android file provider -  authorities should be unique between apps, hence two different app can't have same authorities.
- * 2. Since we are defining provider in library project - If we have defined authorities as constant (like com.downloader.common.provider) for our provider,
+ * 2. Since we are defining provider in library project - If we have defined authorities as constant (like com.downloader.provider.PRDownloaderProvider) for our provider,
  * then our library can't be used by different application.
  * <p>
  * Solution.
@@ -30,11 +47,12 @@ import com.downloader.PRDownloader;
  */
 
 public class PRDownloaderProvider extends ContentProvider {
+
     @Override
     public boolean onCreate() {
         Context context = getContext();
         PRDownloader.initialize(context);
-        return false;
+        return true;
     }
 
     @Nullable
@@ -64,4 +82,18 @@ public class PRDownloaderProvider extends ContentProvider {
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         return 0;
     }
+
+    @Override
+    public void attachInfo(Context context, ProviderInfo providerInfo) {
+        if (providerInfo == null) {
+            throw new NullPointerException("PRDownloaderProvider ProviderInfo cannot be null.");
+        }
+        // So if the authorities equal the library internal ones, the developer forgot to set his applicationId
+        if ("com.downloader.provider.PRDownloaderProvider".equals(providerInfo.authority)) {
+            throw new IllegalStateException("Incorrect provider authority in manifest. Most likely due to a "
+                    + "missing applicationId variable in application\'s build.gradle.");
+        }
+        super.attachInfo(context, providerInfo);
+    }
+
 }
