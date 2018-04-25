9ce0d46d37f45d6e7ff500fd008cd17d5830998c
amitshekhariitbhu <amit.shekhar.iitbhu@gmail.com>
2018-04-25 16:24:53 +0530

    Add support for sync call

---
+++ prdownloader/src/main/java/com/downloader/internal/SynchronousCall.java
@@ -0,0 +1,19 @@
+package com.downloader.internal;
+
+import com.downloader.Response;
+import com.downloader.request.DownloadRequest;
+
+public class SynchronousCall {
+
+    public final DownloadRequest request;
+
+    public SynchronousCall(DownloadRequest request) {
+        this.request = request;
+    }
+
+    public Response execute() {
+        DownloadTask downloadTask = DownloadTask.create(request);
+        return downloadTask.run();
+    }
+
+}
